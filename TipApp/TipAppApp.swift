e599d60ad62994bbd2d94b5836a73975c0b1b247
Vlad <vlad14196@gmail.com>
2023-02-20 12:27:38 +0300

    Initial Commit

---
+++ TipApp/TipAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipAppApp.swift
+//  TipApp
+//
+//  Created by user on 20.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct TipAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
